9b7d154ed57cfa75799b8dd6768cc1dc5cdfa9c0
ajwadhussain-shadman <ajwadhussain38540@gmail.com>
2026-06-11 16:12:47 +0600

    final push

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
   }
 });
  const JWKS = createRemoteJWKSet(
-  new URL('http://localhost:3000/api/auth/jwks')
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
  )
 
 const verification= async(req,res,next)=>{
@@ -183,9 +183,9 @@ async function run() {
    }) 
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
